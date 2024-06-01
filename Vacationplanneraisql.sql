f06cf7903e5212a1f0f2a4ba9128e2b823ca60e3
algo132 <riseoftheneworder@gmail.com>
2024-06-01 13:54:25 -0400

    Add files via upload

---
+++ Vacationplanneraisql.sql
@@ -0,0 +1,13 @@
+my-dynamic-website/
+|-- app/
+|   |-- templates/
+|   |   |-- index.html
+|-- static/
+|   |-- css/
+|   |   |-- style.css
+|-- functions/
+|   |-- main.py
+|   |-- requirements.txt
+|-- public/
+|-- .firebaserc
+|-- firebase.json
